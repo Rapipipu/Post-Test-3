e13e36c0837e496711eb26bd8863756e8b86e4b5
Muhammad Rafi Satria <89404089+Rapipipu@users.noreply.github.com>
2021-11-15 08:58:50 +0700

    Add files via upload

---
+++ Post Test 3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	/*Nama : Muhammad Rafi Satria
+	  NPM  : 2117051046
+	*/
+	
+	int a;
+	
+	cin >> a;
+	
+	if(a == 1){
+	cout << "Satu" << endl;
+	}
+	else if(a == 2){
+	cout << "Dua" << endl;
+	}
+	else if(a == 3){
+	cout << "Tiga" << endl;
+	}
+	else if(a == 4){
+	cout << "Empat" << endl;
+	}
+	else if(a == 5){
+	cout << "Lima" << endl;
+	}
+	else if(a == 6){
+	cout << "Enam" << endl;
+	}
+	else if(a == 7){
+	cout << "Tujuh" << endl;
+	}
+	else if(a == 8){
+	cout << "Delapan" << endl;
+	}
+	else if(a == 9){
+	cout << "Sembilan" << endl;
+	}
+	else{
+	cout << a << endl;
+	}
+}
